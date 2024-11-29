52c168600ec250f16b3b9f30cf8d53f345be4a83
antiti2 <antonia.marsh.araya13@gmail.com>
2024-11-29 17:51:58 -0300

    Update solemneanto.py

--- solemneanto.py
+++ solemneanto.py
@@ -104,9 +104,8 @@ if data is not None:
 sel_colum = st.selectbox("Selecciona una columna para filtrar:", df.select_dtypes(include=['number']).columns, key='unique_key_for_selectbox')
 tipo_grafico = st.selectbox('Selecciona el tipo de gráfico', ['Barras', 'Líneas', 'Área'])
 
-if tipo_grafico == 'Barras':
-    st.write(sel_colum)
-    st.bar_chart(df[sel_colum[0]])
+if tipo_grafico == 'Barras':    
+    st.bar_chart(df[sel_colum])
     
 elif tipo_grafico == 'Líneas':
     st.line_chart(df[sel_colum[0]])
